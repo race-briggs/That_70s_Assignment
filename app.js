c88809a56904e41b33f54795e2e4ce6c838fbfca
Daniel Chiu <cycdaniel@gmail.com>
2019-10-02 13:49:41 -0500

    Added tv axios with responses

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var axios = require('axios');
 function searchApi(query){
   switch(query){
     case 'tv':
+            axios.get('http://api.tvmaze.com/search/shows?q=' + process.argv.slice(3).join("%"))
+            .then(function(response){
+                console.log("Title: " + response.data[0].show.name);
+                console.log("Genres: " + response.data[0].show.genres);
+                console.log("Rating: " + response.data[0].show.rating.average);
+                console.log("Network: " + response.data[0].show.network.name);
+                console.log("Summary: " + response.data[0].show.summary);
+            })
 
       break;
     case 'actor':
@@ -12,3 +20,5 @@ function searchApi(query){
 }
 
 searchApi(process.argv[2]);
+
+
