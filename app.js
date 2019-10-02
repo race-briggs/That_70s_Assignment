7d2581a7b5fa8455dd0b98700f833bc6bc3cf13b
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-02 13:54:20 -0500

    added .txt file

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var axios = require('axios');
+var fs = require('fs');
 
-function searchApi(query){
-  switch(query){
-    case 'tv':
-
-      break;
-    case 'actor':
-
-      break;
-  }
+function searchApi(query) {
+    switch (query) {
+        case 'tv':
+            break;
+        case 'actor':
+            break;
+    }
 }
 
 searchApi(process.argv[2]);
+
+var response;
+
+
+fs.appendFile("log.txt", response, function (err) {
+
+
+    if (err) {
+        console.log(err);
+    }
+
+    else {
+        console.log("Content Added!");
+    }
+
+});
